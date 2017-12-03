42932fae277ebe0ce187954fd25fd33bf002ce6a
Hoang Phan <phan.minhhoang2606@gmail.com>
2017-12-03 16:36:17 -0500

    Change to get Username Attach.

--- public/chat.js
+++ public/chat.js
@@ -3,9 +3,17 @@ window.onload = function(){
 	var field = document.getElementById("field");
 	var sendBtn = document.getElementById("send");
 	var content = document.getElementById("content");
+	var name = document.getElementById("name");
 
 	socket.on('message', function (data){
 		if (data.message) {
+			if (data.username){
+				content.innerHTML += ('<b>'+data.username+':<b/>');
+				console.log('chix');
+			}
+			else{
+				console.log('kochix');
+			}
 			content.innerHTML += (data.message+'<br/>');
 		} else {
 			console.log("There is a problem", data);
@@ -13,7 +21,13 @@ window.onload = function(){
 	});
 	sendBtn.onclick = function(){
 		var text = field.value;
+		var usr = name.value;
 		field.value = "";
-		socket.emit('send', {message: text});
+		if (usr === ""){
+			content.innerHTML += ('<b> PLEASE ENTER YOUR NAME <b/><br/>');
+		}
+		else {
+			socket.emit('send', {message: text, username: usr});
+		}
 	};
 }
